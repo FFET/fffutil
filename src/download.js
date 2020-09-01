596aa44921f083cf48220c182a52d0438ca4fb5b
Jay <jay.wang.scorpion@gmail.com>
2020-09-01 10:06:38 +0800

    download

---
+++ src/download.js
@@ -0,0 +1,17 @@
+/**
+ * download by url
+ *
+ * @author Jay
+ * @since 0.0.1
+ * @param {string} url
+ * @returns {string} Returns the uuid.
+ * @example
+ *
+ * downLoad(url)
+ */
+function download(url) {
+  const element = document.createElement("a");
+  element.download = new Date().getTime();
+  element.href = url;
+  element.click();
+}
